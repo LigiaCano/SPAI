3304562a111d7363d801494adea035a9d65321ab
jesus <j.bernal@upm.es>
2015-09-06 09:38:39 +0200

    Initial

---
+++ git/src/main/java/git/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author jbernal
+ *
+ */
+package git;
--- maven/src/main/java/es/upm/miw/iwvg/log/LoggerDemo.java
+++ maven/src/main/java/es/upm/miw/iwvg/log/LoggerDemo.java
@@ -4,14 +4,19 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class LoggerDemo {
-    public static void main(String[] args) {
-        Logger logger = LogManager.getLogger(LoggerDemo.class.getName());
+
+    public void logs() {
+        Logger logger = LogManager.getLogger(this.getClass().getName());
         logger.fatal("Log de fatal");
         logger.error("Log de error");
         logger.warn("Log de warning");
         logger.info("Log de info");
         logger.debug("Log de debug");
         logger.trace("Log de trace");
+    }
+
+    public static void main(String[] args) {
+        new LoggerDemo().logs();
     }
 
 }
