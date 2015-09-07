edd77e7e27f26d9088d78cb0afc9a5897ec89afc
jesus <j.bernal@upm.es>
2015-09-07 20:51:48 +0200

    update

---
+++ maven/src/main/java/es/upm/miw/iwvg/test/NaturalDao.java
@@ -0,0 +1,13 @@
+package es.upm.miw.iwvg.test;
+
+public class NaturalDao {
+    
+    public void save(Natural natural){
+        //Se have persistente
+    }
+    
+    public Natural findOne(Integer id){
+        return null;
+    }
+
+}
